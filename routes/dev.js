d3b8a5efc58594a2ce8782c5242b4f23c5c67853
ABHISHEK KUMAR <72004395+Abhishekkumar2001@users.noreply.github.com>
2023-07-17 01:18:32 +0530

    Read operation Done

--- routes/dev.js
+++ routes/dev.js
@@ -7,12 +7,34 @@ const Developer = require("../models/developer");
 router.post("/create", async (req, res) =>{
     try{
         const newDeveloper = await Developer.create(req.body);
-        res.status(201).json({message : "Success!"})
+        res.status(201).json({message : "Success!"});
     }
     catch(err){
-        res.status(400).json({message : "Error!"})
+        res.status(400).json({message : "Error!"});
     }
 });
+
+//get all the developers
+router.get("/all", async (req, res) => {
+    try{
+        const allDevelopers = await Developer.find();
+        res.status(201).json(allDevelopers);
+    }
+    catch(err){
+        res.status(400).json({message : "Error!"});
+    }
+})
+
+// get one the developer
+router.get("/:id", async (req, res) => {
+    try{
+        const developer = await Developer.findById(req.params.id);
+        res.status(400).json(developer);
+    }
+    catch(err){
+        res.status(400).json({message : "Error!"});
+    }
+})
 
 
 module.exports = router;
