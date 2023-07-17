df08358534c0b149f371e0a6daa99c85dc524904
ABHISHEK KUMAR <72004395+Abhishekkumar2001@users.noreply.github.com>
2023-07-17 10:24:19 +0530

    Update Operation Done

--- routes/dev.js
+++ routes/dev.js
@@ -4,35 +4,49 @@ const Developer = require("../models/developer");
 
 
 // Create the developer
-router.post("/create", async (req, res) =>{
-    try{
+router.post("/create", async (req, res) => {
+    try {
         const newDeveloper = await Developer.create(req.body);
-        res.status(201).json({message : "Success!"});
+        res.status(201).json({ message: "Success!" });
     }
-    catch(err){
-        res.status(400).json({message : "Error!"});
+    catch (err) {
+        res.status(400).json({ message: "Error!" });
     }
 });
 
 //get all the developers
 router.get("/all", async (req, res) => {
-    try{
+    try {
         const allDevelopers = await Developer.find();
         res.status(201).json(allDevelopers);
     }
-    catch(err){
-        res.status(400).json({message : "Error!"});
+    catch (err) {
+        res.status(400).json({ message: "Error!" });
     }
 })
 
 // get one the developer
 router.get("/:id", async (req, res) => {
-    try{
+    try {
         const developer = await Developer.findById(req.params.id);
-        res.status(400).json(developer);
+        res.status(201).json(developer);
     }
-    catch(err){
-        res.status(400).json({message : "Error!"});
+    catch (err) {
+        res.status(400).json({ message: "Error!" });
+    }
+})
+
+//Update the developer
+router.patch("/update/:id", async (req, res) => {
+    try {
+        const { id: _id } = await req.params;
+        const changes = await req.body;
+
+        const updatedDeveloper = await Developer.findByIdAndUpdate(_id, { ...changes, _id });
+        res.status(201).json({ message: "Success!" });
+    }
+    catch (err) {
+        res.status(400).json({ message: "Error!" });
     }
 })
 
